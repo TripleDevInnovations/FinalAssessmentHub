6340e29ffcf5165a5ed8a5fbe985609711901df4
JanekStreet <igorrjackoo@gmail.com>
2025-11-05 12:00:13 +0100

    application build

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
-from backend.app.controller import exam_controller
+from backend.app.controller import exam_controller, root_controller
 
 app = FastAPI()
 
@@ -22,3 +24,8 @@ app.add_middleware(
 
 
 app.include_router(exam_controller.router, prefix="/exam", tags=["Exam"])
+app.include_router(root_controller.router, tags=["Root"])
+
+if __name__ == "__main__":
+    port = int(os.getenv("BACKEND_PORT", 8000))
+    uvicorn.run(app, host="127.0.0.1", port=port)
